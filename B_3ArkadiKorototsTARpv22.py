ca5f41b0774a238ddcb55945327917ed501fa509
opilane <opilane@HP-CZC2349HT9>
2023-02-15 14:43:03 +0200

    Add project files.

---
+++ B_3ArkadiKorototsTARpv22.py
@@ -0,0 +1,38 @@
+﻿#Arkadi Korotitsh TARpv22
+
+from модуль import *
+
+cities = []
+populations = []
+print("сделано Arkadi Korotitsh TARpv22")
+while True:
+    city = input('Введите название города. ')
+    population = int(input('Введите население этого города. '))
+    cities.append(city)
+    populations.append(population)
+    escape = input('Введите "e" для завершения ввода данных о городах \
+или Enter для продолжения. ')
+    if escape.lower() == 'e':
+        break
+
+city_dict = dict(zip(cities, populations))
+
+
+while True:
+    command = input('Команды: \n1-Узнать, вводя название города,  сколько в нем жителей\n2-Отобразить в алфавитном порядке список городов и количество жителей\n3-Найти населенность города, введя его название\n4-Найти города в которых живет меньше, чем n жителей\n5-найти город с самым высоким населением\n6-выйти\n')
+    if command == '1':
+        city = input('Введите название города. ')
+        print(get_population(city, city_dict))
+    if command == '3':
+        city = int(input('Введите количество населения. '))
+        print(get_city(city,populations, city_dict))
+    if command == '2':
+        print(show_inf(city_dict))
+    if command == '5':
+        print(most_pop(city_dict))
+    if command == '4':
+        num = int(input('Введите количество населения. '))
+        print(less(city_dict, num))
+    if command == '6':
+        break
+        
---
+++ модуль.py
@@ -0,0 +1,31 @@
+﻿def get_population(city, cities):
+    if city in cities.keys():
+        return f'Популяция {city} равна {cities[city]}.'
+    else:
+        return 'Такого города нет в списке.' 
+
+def get_city(population, cities, populations):
+    if population in populations.keys():
+        return f'Популяция {population} равна {cities[population]}.'
+    else:
+        return f'Популяция {population} равна {cities[population]}.'
+
+def show_inf(cities):
+    sorted_tuple = sorted(cities.items())
+    return sorted_tuple 
+
+def most_pop(cities):
+    big = 0
+    town = ''
+    for city, pop in cities.items():
+        if pop > big:
+            big = pop
+            town = city
+    return f'{town} : {big}'
+
+def less(cities, num):
+    my_dic = {}
+    for city, pop in cities.items():
+        if pop < num:
+            my_dic[city] = pop
+    return my_dic
